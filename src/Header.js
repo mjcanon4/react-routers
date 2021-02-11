b2ac79749087724fb2ed1c301a1d3c45cc08e553
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-02-11 00:05:35 -0500

    final, toyed around with react router

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-//Link uses HTML5 history API to achieve the client-side routing functionality WITHOUT a page refresh
+import { NavLink } from "react-router-dom";
+//NavLink uses HTML5 history API to achieve the client-side routing functionality WITHOUT a page refresh
 
 const Header = () => {
   return (
     <ul className="navigation-menu">
       <li>
-        <Link to="/">Home</Link>
+        <NavLink to="/" activeClassName="active" exact={true}>
+          Home
+        </NavLink>
       </li>
       <li>
-        <Link to="/about">About</Link>
+        <NavLink to="/about" activeClassName="active">
+          About
+        </NavLink>
       </li>
       <li>
-        <Link to="/contact">Contact</Link>
+        <NavLink to="/contact" activeClassName="active">
+          Contact
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile" activeClassName="active">
+          Profile
+        </NavLink>
       </li>
     </ul>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import "./styles.css";
 
@@ -16,8 +16,23 @@ const Contact = () => {
   return <h2>Contact Page</h2>;
 };
 
+const Profile = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <Link to="/profile/10">Edit Profile</Link>
+    </div>
+  );
+};
+
+const EditProfile = (props) => {
+  console.log(props);
+  return <p>Editing profile page of user with id {props.match.params.id}</p>;
+};
+
 const NotFound = () => {
-  return <h2>Page Not Found</h2>;
+  return <h2>Page Not found.</h2>;
 };
 
 const App = () => {
@@ -28,6 +43,8 @@ const App = () => {
         <Route path="/" component={Home} exact={true} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/profile" component={Profile} exact={true} />
+        <Route path="/profile/:id" component={EditProfile} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
